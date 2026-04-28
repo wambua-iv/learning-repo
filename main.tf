bb29fba4b58d0adfd34e58850f6f9ad1d38825f0
Wambua Musalu <47148208+wambua-iv@users.noreply.github.com>
2026-04-28 15:22:11 +0300

    ci update

--- main.tf
+++ main.tf
@@ -7,3 +7,13 @@ resource "azurerm_resource_group" "dep1" {
     name = "ghrg1"
     location = "uksouth"
 }
+
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Create a VPC
+resource "aws_vpc" "aws-tf-task" {
+  cidr_block = "10.0.0.0/16"
+}
